f522f69bbd73006e47a409086ffce1d22655efda
zerratar <zerratar@gmail.com>
2025-07-11 18:15:06 +0200

    allow setting input argument with target directory to watch

--- src/LanSync/Program.cs
+++ src/LanSync/Program.cs
@@ -4,12 +4,54 @@
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Simple LAN Sync - Enter the full path of the folder to sync:");
-            string folder = Console.ReadLine();
-            if (!Directory.Exists(folder))
+            Console.WriteLine("Simple LAN Sync");
+            var folder = string.Empty;
+            if (args.Length > 0)
             {
-                Console.WriteLine("Folder does not exist.");
-                return;
+                // If a folder is provided as a command line argument, use it
+                if (Directory.Exists(args[0]))
+                {
+                    folder = args[0];
+                    Console.WriteLine($"Using folder: {args[0]}");
+                
+                }
+                else
+                {
+                    Console.WriteLine("Provided folder does not exist.");
+                }
+            }
+
+            if (string.IsNullOrEmpty(folder))
+            {
+                // Otherwise, prompt for the folder
+                Console.WriteLine("Enter the full path of the folder to sync: ");
+                Console.Write("> ");
+                folder = Console.ReadLine();
+                if (!Directory.Exists(folder))
+                {
+                    Console.WriteLine("Folder does not exist, do you want to create it? [Y/N]");
+                    Console.Write("> ");
+
+                    var response = Console.ReadLine()?.Trim().ToUpper();
+                    if (response == "Y" || response == "YES")
+                    {
+                        try
+                        {
+                            Directory.CreateDirectory(folder);
+                            Console.WriteLine($"Created folder: {folder}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Failed to create folder: {ex.Message}");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Exiting...");
+                        return;
+                    }
+                }
             }
 
             var app = new LanSyncApp(folder, 42042); // 42042 is our sync port
